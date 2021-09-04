56a224b3580a24c822063517a971e8f776c06c31
ahmed kandil <ahmedkandil2015@hotmail.com>
2021-09-04 20:41:10 +0200

    update laravel version with issues

--- resources/views/admin/partail/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<footer class="main-footer">
-    <!-- To the right -->
-    <div class="pull-right hidden-xs">
-        Inform Me
-    </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; 2016 <a href="#">Inform Me</a>.</strong> All rights reserved.
-</footer>
